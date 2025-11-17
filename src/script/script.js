a5044b69e6c5350b24baa8166558bf3e492661ec
renatoMarqs <renatoxyz007@gmail.com>
2025-11-17 04:11:37 -0300

    A minimização do menu lateral concluída;  responsividade até o ipad mini concluida; o menu de cadastrar relatório foi criado; foi feita a Criação do arquivo script.js e a remoção do botão de cadastrar relatório

---
+++ src/script/script.js
@@ -0,0 +1,31 @@
+// -------- MENU RETRÁTIL --------
+    const menu = document.getElementById("menu_lateral");
+    const toggleMenu = document.getElementById("toggleMenu");
+
+    toggleMenu.addEventListener("click", () => {
+        menu.classList.toggle("mini");
+    });
+
+
+// ======== modal ========
+const modal = document.getElementById("modalCadastro");
+const btnAbrir = document.querySelector("#menu_lateral nav li:nth-child(2)");
+const btnFechar = document.getElementById("fecharModal");
+
+// ======== abrir modal ========
+btnAbrir.addEventListener("click", () => {
+    modal.style.display = "flex";
+});
+
+// ======== fechar modal ========
+btnFechar.addEventListener("click", () => {
+    modal.style.display = "none";
+});
+
+// ======== fechar ao clicar fora ========
+window.addEventListener("click", (event) => {
+    if (event.target === modal) {
+        modal.style.display = "none";
+    }
+});
+
